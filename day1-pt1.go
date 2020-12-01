82af83b17df7438a8c83446ea073e445fddfc9ff
Chris Soliday <solidaycl@gmail.com>
2020-12-01 08:13:54 -0500

    Day 1.

---
+++ day1-pt1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+    "strconv"
+)
+ 
+func main() {
+	file, err := os.Open("day1.input")
+ 
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+ 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []int
+ 
+	for scanner.Scan() {
+        i, _ := strconv.Atoi(scanner.Text())
+		txtlines = append(txtlines, i)
+	}
+ 
+	file.Close()
+ 
+	for _, i := range txtlines {
+        for _, j := range txtlines {
+            if(i + j == 2020) {
+                fmt.Println(i*j)
+            }
+        }
+	}
+}
+
---
+++ day1-pt2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+    "strconv"
+)
+
+func main() {
+	file, err := os.Open("day1.input")
+ 
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+ 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []int
+ 
+	for scanner.Scan() {
+        i, _ := strconv.Atoi(scanner.Text())
+		txtlines = append(txtlines, i)
+	}
+ 
+	file.Close()
+ 
+	for _, i := range txtlines {
+        for _, j := range txtlines {
+            for _, k := range txtlines {
+                if(i + j + k== 2020) {
+                    fmt.Println(i*j*k)
+                }
+            }
+        }
+	}
+}
+
